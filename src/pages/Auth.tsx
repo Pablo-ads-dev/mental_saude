1136c2c3381c187febf8751e2deb8fe1b6704a97
Pablo-ads-dev <pabloadsdev@gmail.com>
2026-03-09 17:05:27 -0300

    correção

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { Heart, Mail, Lock, User, ArrowLeft } from "lucide-react";
 import { supabase } from "@/integrations/supabase/client";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/authContext";
+import { useAuth } from "@/context/authContext";
 
 import { useEffect } from "react";
 
